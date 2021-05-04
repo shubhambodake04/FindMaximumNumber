fc02201c4f09dffbc602065bffbfe4fae1ba1233
shubhambodake04 <shubhambodake6476@gmail.com>
2021-05-04 21:27:46 +0530

    Add project files.

---
+++ MaximunNumber_MSTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FindMaximumNumber;
+
+namespace MaximunNumber_MSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MaximumInteger()
+        {
+
+        }
+    }
+}
